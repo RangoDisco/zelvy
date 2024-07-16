c0c1a3b88669acf9477fbb5f62a47326f70f4c37
RangoDisco <diasmaxime1@gmail.com>
2024-07-16 20:12:29 +0200

    feat(server): added goal type enum

---
+++ server/enums/goal.go
@@ -0,0 +1,9 @@
+package enums
+
+const (
+	KcalBurned           = "kcalBurned"
+	KcalConsumed         = "kcalConsumed"
+	MilliliterDrank      = "milliliterDrank"
+	MainWorkoutDuration  = "mainWorkoutDuration"
+	ExtraWorkoutDuration = "extraWorkoutDuration"
+)
