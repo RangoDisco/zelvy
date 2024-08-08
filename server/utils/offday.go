a6c030ada56c7c22ec2b316ced375401d03d03b8
RangoDisco <diasmaxime1@gmail.com>
2024-08-08 14:18:44 +0200

    fix(utils): renamed helpers to utils and fix offdays

---
+++ server/utils/offday.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"github.com/google/uuid"
+	"github.com/rangodisco/zelby/server/database"
+	"github.com/rangodisco/zelby/server/models"
+	"time"
+)
+
+func FetchByGoalAndDate(goalId uuid.UUID) (*models.Offday, error) {
+	var offDay models.Offday
+	sod := time.Now().Truncate(24 * time.Hour)
+	eod := sod.Add(24 * time.Hour)
+	if err := database.DB.First(&offDay, "goal_id = ? AND day >= ? AND day < ?", goalId, sod, eod).Error; err != nil {
+		return nil, err
+	}
+
+	return &offDay, nil
+}
