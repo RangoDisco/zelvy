0d234dedf624605586343cb0dafd9da3a04a2266
RangoDisco <diasmaxime1@gmail.com>
2024-08-08 15:59:58 +0200

    feat(bot): removed now useless "PickWinner" util

--- bot/utils/summary.go
+++ bot/utils/summary.go
@@ -1,10 +1,7 @@
 package utils
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
-	"github.com/bwmarrin/discordgo"
 	"io"
 	"log"
 	"net/http"
@@ -99,72 +96,4 @@ func IsSuccess(metrics []Metric) bool {
 		}
 	}
 	return true
-}
-
-/**
- * Pick a winner from the list of user ids
- * Happens every day, even if the goals are met
- */
-func PickWinner(s *discordgo.Session, summary Summary) (*discordgo.User, error) {
-	baseUrl := os.Getenv("BASE_URL")
-	apiKey := os.Getenv("API_KEY")
-
-	fmt.Printf("Picking winner for summary ID: %s\n", summary.ID)
-
-	body := map[string]string{
-		"summaryId": summary.ID,
-	}
-
-	jsonBody, err := json.Marshal(body)
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling request body: %v", err)
-	}
-
-	req, err := http.NewRequest("POST", baseUrl+"/api/users/pick-winner", bytes.NewBuffer(jsonBody))
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %v", err)
-	}
-
-	// Add api key and content type to headers
-	req.Header.Add("X-API-KEY", apiKey)
-	req.Header.Add("Content-Type", "application/json")
-
-	// Create client & send request
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("error sending request: %v", err)
-	}
-	defer res.Body.Close()
-
-	fmt.Printf("Response status: %s\n", res.StatusCode)
-
-	respBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %v", err)
-	}
-
-	// Check if the response is JSON
-	var response struct {
-		Winner struct {
-			DiscordID string `json:"discordID"`
-		} `json:"winner"`
-	}
-	if err := json.Unmarshal(respBody, &response); err != nil {
-		// If it's not JSON, assume the response is just the winner ID
-		response.Winner.DiscordID = string(respBody)
-	}
-
-	if response.Winner.DiscordID == "" {
-		return nil, fmt.Errorf("no winner ID received")
-	}
-
-	fmt.Printf("Winner ID: %s\n", response.Winner.DiscordID)
-
-	winner, err := s.User(response.Winner.DiscordID)
-	if err != nil {
-		return nil, fmt.Errorf("error fetching user: %v", err)
-	}
-
-	return winner, nil
 }
