d1ce79b92d12f9baeed2faf929cb3bdda93adc3f
RangoDisco <diasmaxime1@gmail.com>
2024-08-08 19:42:16 +0200

    WIP

--- server/main.go
+++ server/main.go
@@ -4,9 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/rangodisco/zelby/server/database"
-	"github.com/rangodisco/zelby/server/middlewares"
 	"github.com/rangodisco/zelby/server/routes"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -27,8 +27,19 @@ func main() {
 	// Start gin server
 	r := gin.Default()
 
+	r.LoadHTMLGlob("templates/*.html")
+
 	// Middleware to check API key in header
-	r.Use(middlewares.CheckKey())
+	//r.Use(middlewares.CheckKey())
+
+	// Serve static files
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.POST("/clicked", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "clicked.html", nil)
+	})
 
 	// Register routes from routes package
 	routes.RegisterSummaryRoutes(r)
