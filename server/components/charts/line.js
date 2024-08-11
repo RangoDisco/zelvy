d9ca971dde19feded6c5bbf596510a7e1523f78b
RangoDisco <diasmaxime1@gmail.com>
2024-08-11 15:40:09 +0200

    feat(template): added first blocks for home page charts

---
+++ server/components/charts/line.js
@@ -0,0 +1,60 @@
+const createLine = (ctx, line) => {
+  return new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: line.labels,
+      datasets: buildDataSet(line.datasets),
+    },
+    options: {
+      layout: {
+        padding: 10,
+      },
+      scales: {
+        x: {
+          grid: {
+            color: "rgba(255, 255, 255, 0.3)",
+          },
+
+          ticks: {
+            color: "rgba(255, 255, 255, 0.5)",
+          },
+        },
+        y: {
+          grid: {
+            color: "rgba(255, 255, 255, 0.3)",
+          },
+
+          ticks: {
+            color: "rgba(255, 255, 255, 0.5)",
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          position: "bottom",
+          labels: {
+            color: "rgba(255, 255, 255, 0.5)",
+          },
+        },
+      },
+    },
+  });
+};
+
+const buildDataSet = (rawDatasets) => {
+  const sets = [];
+  rawDatasets.forEach((set) => {
+    sets.push({
+      label: set.label,
+      data: set.data,
+      fill: true,
+      backgroundColor: set.backgroundColor,
+      borderColor: set.borderColor,
+      borderWidth: 1,
+      pointBackgroundColor: set.pointBackgroundColor,
+      pointBorderColor: "#fff",
+    });
+  });
+
+  return sets;
+};
