5dc7ba1074adde0dbfa016a959aadcd151d5d80f
RangoDisco <diasmaxime1@gmail.com>
2024-08-11 15:40:09 +0200

    feat(template): added first blocks for home page charts

---
+++ server/components/charts/radar.js
@@ -0,0 +1,58 @@
+const createRadar = (ctx, radar) => {
+  return new Chart(ctx, {
+    type: "radar",
+    data: {
+      labels: radar.labels,
+      datasets: buildDataSet(radar.datasets),
+    },
+    options: {
+      layout: {
+        padding: 10,
+      },
+      scales: {
+        r: {
+          ticks: {
+            display: false,
+          },
+          angleLines: {
+            display: false,
+            color: "rgba(255, 255, 255, 0.2)",
+          },
+          grid: {
+            color: "rgba(255, 255, 255, 0.2)",
+          },
+          pointLabels: {
+            font: 12,
+            color: "rgba(255, 255, 255, 0.5)",
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          position: "bottom",
+          labels: {
+            color: "rgba(255, 255, 255, 0.5)",
+          },
+        },
+      },
+    },
+  });
+};
+
+const buildDataSet = (rawDatasets) => {
+  const sets = [];
+  rawDatasets.forEach((set) => {
+    sets.push({
+      label: set.label,
+      data: set.data,
+      fill: true,
+      backgroundColor: set.backgroundColor,
+      borderColor: set.borderColor,
+      borderWidth: 1,
+      pointBackgroundColor: set.pointBackgroundColor,
+      pointBorderColor: "#fff",
+    });
+  });
+
+  return sets;
+};
