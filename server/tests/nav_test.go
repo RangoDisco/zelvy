cd61c498f6e5277d1d16112b869a511042af52d2
RangoDisco <diasmaxime1@gmail.com>
2024-08-15 14:54:54 +0200

    feat(server): added nav template test and updated tempalte accordingly

---
+++ server/tests/nav_test.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/rangodisco/zelby/server/components"
+)
+
+func TestNav(t *testing.T) {
+	r, w := io.Pipe()
+
+	go func() {
+		_ = components.Navbar().Render(context.Background(), w)
+		_ = w.Close()
+	}()
+
+	doc, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		t.Fatalf("Failed to read template: %v", err)
+	}
+
+	// Expecting 1 navbar
+	if doc.Find(`[data-testid="navTemplate"]`).Length() == 0 {
+		t.Error("Navbar not found")
+	}
+}
