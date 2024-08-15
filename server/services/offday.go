6f60ef774445765ac7608ba3f5e31e443b48a124
RangoDisco <diasmaxime1@gmail.com>
2024-08-15 05:28:25 +0200

    fix(server): set offday to false is nothing is found in db

--- server/services/offday.go
+++ server/services/offday.go
@@ -13,7 +13,7 @@ func IsOff(goalId uuid.UUID) bool {
 	sod := time.Now().Truncate(24 * time.Hour)
 	eod := sod.Add(24 * time.Hour)
 	if err := database.DB.First(&offDay, "goal_id = ? AND day >= ? AND day < ?", goalId, sod, eod).Error; err != nil {
-		return true
+		return false
 	}
 
 	if offDay.ID == uuid.Nil {
