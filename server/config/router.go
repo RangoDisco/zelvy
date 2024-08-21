bddd93944e1c2116badd4439dd8ad5e3c17cb53c
RangoDisco <diasmaxime1@gmail.com>
2024-08-21 20:29:39 +0200

    chore(server): moved config logic to new config package

---
+++ server/config/router.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rangodisco/zelby/server/components"
+	"github.com/rangodisco/zelby/server/gintemplrenderer"
+	"github.com/rangodisco/zelby/server/handlers"
+	"github.com/rangodisco/zelby/server/middlewares"
+)
+
+func SetupRouter() *gin.Engine {
+	// Start gin server
+	r := gin.Default()
+
+	ginHtmlRenderer := r.HTMLRender
+	r.HTMLRender = &gintemplrenderer.HTMLTemplRenderer{FallbackHtmlRenderer: ginHtmlRenderer}
+
+	// Middleware to check API key in header
+	r.Use(middlewares.CheckKey())
+
+	// Register handlers from handlers package
+	handlers.RegisterSummaryRoutes(r)
+	handlers.RegisterGoalRoutes(r)
+	handlers.RegisterOffDayRoutes(r)
+	handlers.RegisterUserRoutes(r)
+	handlers.RegisterChartRoutes(r)
+
+	// Serve static files
+	r.Static("/assets", "./assets")
+
+	// Handle 404
+	r.NoRoute(func(c *gin.Context) {
+		r := gintemplrenderer.New(c.Request.Context(), http.StatusNotFound, components.NotFound())
+		c.Render(404, r)
+	})
+
+	return r
+}
