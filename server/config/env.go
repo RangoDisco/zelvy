e4f837e00ef80802d29eff8d1582cd61088840ce
RangoDisco <diasmaxime1@gmail.com>
2024-08-21 20:29:39 +0200

    chore(server): moved config logic to new config package

---
+++ server/config/env.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() error {
+	// Get the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("error getting current working directory: %w", err)
+	}
+
+	// Load main .env file
+	mainEnvPath := filepath.Join(cwd, ".env")
+	if err := godotenv.Load(mainEnvPath); err != nil {
+		return fmt.Errorf("error loading .env file: %w", err)
+	}
+
+	mode := os.Getenv("GIN_MODE")
+	var envFile string
+
+	switch mode {
+	case "test":
+		envFile = ".env.test"
+	case "release":
+		envFile = ".env.prod"
+	default:
+		return nil
+	}
+
+	additionalEnvPath := filepath.Join(cwd, envFile)
+
+	// Check if file exists
+	if _, err := os.Stat(additionalEnvPath); os.IsNotExist(err) {
+		return fmt.Errorf("additional env file does not exist: %s", additionalEnvPath)
+	}
+
+	// Overwrites potential duplicate variables
+	if err := godotenv.Overload(additionalEnvPath); err != nil {
+		return fmt.Errorf("error loading additional env file: %w", err)
+	}
+
+	return nil
+}
