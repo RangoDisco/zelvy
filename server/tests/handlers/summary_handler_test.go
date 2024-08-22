62539935867354ec05f4a28e5ab387fd3c7b1d7b
RangoDisco <diasmaxime1@gmail.com>
2024-08-22 17:14:27 +0200

    feat(server): added get summary test

--- server/tests/handlers/summary_handler_test.go
+++ server/tests/handlers/summary_handler_test.go
@@ -6,15 +6,15 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/rangodisco/zelby/server/database"
 	"github.com/rangodisco/zelby/server/tests"
 	"github.com/rangodisco/zelby/server/tests/factories"
+	"github.com/rangodisco/zelby/server/types"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAddSummary(t *testing.T) {
-	// Setup router
-
 	// Create example input model
 	summaryToCreate := factories.CreateSummaryInputModel()
 
@@ -27,5 +27,29 @@ func TestAddSummary(t *testing.T) {
 	tests.Router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusCreated, w.Code)
+}
 
+func TestGetSummary(t *testing.T) {
+	// Insert summary in db
+	summary := factories.CreateSummaryModel()
+	res := database.GetDB().Create(&summary)
+	assert.NoError(t, res.Error)
+
+	// Try fetching latest summary
+	w := httptest.NewRecorder()
+	req := SendRequest("GET", "/summaries", nil)
+
+	tests.Router.ServeHTTP(w, req)
+
+	// Ensure that the response is OK
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// Ensure that the response is a JSON
+	assert.Equal(t, "application/json; charset=utf-8", w.Header().Get("Content-Type"))
+
+	// Ensure that the response is the correct summary
+	var summaryResponse types.SummaryViewModel
+	err := json.Unmarshal(w.Body.Bytes(), &summaryResponse)
+	assert.NoError(t, err)
+	assert.Equal(t, summary.ID.String(), summaryResponse.ID)
 }
