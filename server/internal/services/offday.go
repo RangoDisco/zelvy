489d17141d07c2b4547c2f651a6df6697adf4c72
RangoDisco <diasmaxime1@gmail.com>
2025-05-14 17:42:53 +0200

    fix(server): properly fetched off day from db

--- server/internal/services/offday.go
+++ server/internal/services/offday.go
@@ -13,11 +13,12 @@ func IsOff(goalId uuid.UUID) bool {
 	var offDay models.Offday
 	sod := time.Now().Truncate(24 * time.Hour)
 	eod := sod.Add(24 * time.Hour)
-	if err := database.GetDB().First(&offDay, "goal_id = ? AND day >= ? AND day < ?", goalId, sod, eod).Error; err != nil {
+
+	if err := database.GetDB().Where("goal_id = ? AND day >= ? AND day < ?", goalId, sod, eod).Find(&offDay).Error; err != nil {
 		return false
 	}
 
-	if offDay.ID == uuid.Nil {
+	if offDay.ID != uuid.Nil {
 		return true
 	}
 
