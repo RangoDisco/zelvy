572e8cc2ade593450f5087e4ce298382dfc376f5
RangoDisco <diasmaxime1@gmail.com>
2025-06-05 07:57:12 +0200

    fix(server): updated water metric name

--- server/internal/enums/goal.go
+++ server/internal/enums/goal.go
@@ -3,8 +3,7 @@ package enums
 const (
 	KcalBurned           = "KCAL_BURNED"
 	KcalConsumed         = "KCAL_CONSUMED"
-	MilliliterDrank      = "LITTER_DRANK"
+	MilliliterDrank      = "MILLILITER_DRANK"
 	MainWorkoutDuration  = "MAIN_WORKOUT_DURATION"
 	ExtraWorkoutDuration = "EXTRA_WORKOUT_DURATION"
-	TotalSets            = "TOTAL_SETS"
 )
