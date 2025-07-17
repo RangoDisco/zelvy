d1eec7545c54a96874c77029ff237b7755ab31de
RangoDisco <diasmaxime1@gmail.com>
2025-07-17 08:15:13 +0200

    chore(workout): lowercased function when they weren't exported

--- server/internal/services/workout.go
+++ server/internal/services/workout.go
@@ -11,34 +11,6 @@ import (
 	"github.com/google/uuid"
 )
 
-func FetchChartWorkouts() ([]models.Workout, []models.Workout, error) {
-	// Get dates of the current week
-	sow := time.Now().Add(-168 * time.Hour)
-	eow := time.Now()
-
-	// Get the number of workouts between 7 days ago and now
-	thisWeek, err := FetchWorkoutsByDateRange(sow, eow)
-	if err != nil {
-		return []models.Workout{}, []models.Workout{}, err
-	}
-
-	// Get the number of workouts between 14 days ago and 7 days ago
-	lastWeek, err := FetchWorkoutsByDateRange(sow.Add(-168*time.Hour), sow)
-	if err != nil {
-		return []models.Workout{}, []models.Workout{}, err
-	}
-
-	return thisWeek, lastWeek, nil
-
-}
-
-// FetchWorkoutsByDateRange fetches all the workouts in the database between two dates
-func FetchWorkoutsByDateRange(startDate time.Time, endDate time.Time) ([]models.Workout, error) {
-	var workouts []models.Workout
-	err := database.GetDB().Raw("SELECT * FROM workouts w INNER JOIN summaries s ON w.summary_id = s.id WHERE s.date >= ? AND s.date < ?", startDate, endDate).Scan(&workouts).Error
-	return workouts, err
-}
-
 // ConvertToWorkoutModel converts a WorkoutInputModel to a Workout model (used when creating a new workout)
 func ConvertToWorkoutModel(w *types.WorkoutInputModel, summaryId uuid.UUID) models.Workout {
 	return models.Workout{
@@ -51,38 +23,35 @@ func ConvertToWorkoutModel(w *types.WorkoutInputModel, summaryId uuid.UUID) mode
 	}
 }
 
-// ConvertToWorkoutViewModel converts a Workout model to a WorkoutViewModel (used in the front)
-func ConvertToWorkoutViewModel(w *models.Workout) types.WorkoutViewModel {
+// convertToWorkoutViewModel used when fetching a summary, converts a Workout model to a WorkoutViewModel
+func convertToWorkoutViewModel(w *models.Workout) types.WorkoutViewModel {
 	return types.WorkoutViewModel{
 		ID:           w.ID.String(),
 		KcalBurned:   w.KcalBurned,
 		ActivityType: w.ActivityType,
 		Name:         w.Name,
-		Duration:     ConvertMsToHour(w.Duration),
+		Duration:     convertMsToHour(w.Duration),
 		Picto:        getWorkoutPicto(w.ActivityType),
 	}
 }
 
-// CalculateMainWorkoutDuration sums the duration of all the gym sessions
-func CalculateMainWorkoutDuration(workouts *[]models.Workout) float64 {
+// calculateWorkoutDuration used when fetching a summary, add the duration of all the workout from a given type
+func calculateWorkoutDuration(w *[]models.Workout, target string) float64 {
 	var duration float64
-	for _, w := range *workouts {
-		if w.ActivityType == "strength" {
+	for _, w := range *w {
+		if target == enums.MainWorkoutDuration && w.ActivityType == "strength" {
 			duration = duration + w.Duration
 		}
 	}
+
 	return duration
 }
 
-// CalculateExtraWorkoutDuration sums the duration of all cardio sessions
-func CalculateExtraWorkoutDuration(workouts *[]models.Workout) float64 {
-	var duration float64
-	for _, w := range *workouts {
-		if w.ActivityType != "strength" {
-			duration = duration + w.Duration
-		}
-	}
-	return duration
+// fetchWorkoutsByDateRange fetches all the workouts in the database between two dates
+func fetchWorkoutsByDateRange(startDate time.Time, endDate time.Time) ([]models.Workout, error) {
+	var workouts []models.Workout
+	err := database.GetDB().Raw("SELECT * FROM workouts w INNER JOIN summaries s ON w.summary_id = s.id WHERE s.date >= ? AND s.date < ?", startDate, endDate).Scan(&workouts).Error
+	return workouts, err
 }
 
 func getWorkoutPicto(activityType string) string {
@@ -94,7 +63,7 @@ func getWorkoutPicto(activityType string) string {
 	}
 }
 
-// Handles name based on activity type in case null
+// Handles name based on the activity's type in case null
 func getWorkoutName(w *types.WorkoutInputModel) string {
 	if w.Name != "" {
 		return w.Name
@@ -102,14 +71,35 @@ func getWorkoutName(w *types.WorkoutInputModel) string {
 
 	switch w.ActivityType {
 	case enums.WorkoutTypeStrength:
-		return "Séance de musculation"
+		return "Gym"
 	case enums.WorkoutTypeRunning:
-		return "Footing"
+		return "Running"
 	case enums.WorkoutTypeCycling:
-		return "Vélo"
+		return "Cycling"
 	case enums.WorkoutTypeWalking:
-		return "Marche"
+		return "Walking"
 	default:
-		return "Séance de sport"
+		return "Random silly workout"
 	}
+}
+
+func fetchChartWorkouts() ([]models.Workout, []models.Workout, error) {
+	// Get dates of the current week
+	sow := time.Now().Add(-168 * time.Hour)
+	eow := time.Now()
+
+	// Get the number of workouts during the last 7 days
+	thisWeek, err := fetchWorkoutsByDateRange(sow, eow)
+	if err != nil {
+		return []models.Workout{}, []models.Workout{}, err
+	}
+
+	// Get the number of workouts between 14 days ago and 7 days ago
+	lastWeek, err := fetchWorkoutsByDateRange(sow.Add(-168*time.Hour), sow)
+	if err != nil {
+		return []models.Workout{}, []models.Workout{}, err
+	}
+
+	return thisWeek, lastWeek, nil
+
 }
