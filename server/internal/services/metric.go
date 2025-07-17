5ff21193c44832cd53e7263073dda9b48a150bf6
RangoDisco <diasmaxime1@gmail.com>
2025-07-17 08:12:19 +0200

    feat(goal): created service and moved related function to it

--- server/internal/services/metric.go
+++ server/internal/services/metric.go
@@ -2,9 +2,6 @@ package services
 
 import (
 	"server/config/database"
-	"slices"
-	"strconv"
-
 	"server/internal/enums"
 	"server/internal/models"
 	"server/pkg/types"
@@ -28,65 +25,6 @@ func ConvertToMetricModel(m *types.MetricInputModel, summaryId uuid.UUID) (model
 	}, true
 }
 
-// Determine if the metric is successful based on the threshold
-func IsMetricSuccessful(value, goalValue float64, comparison string, isOffDay bool) bool {
-	// In case the day is off, goal is always achieved
-	if isOffDay {
-		return true
-	}
-	switch comparison {
-	case "greater":
-		return value >= goalValue
-	case "less":
-		return value <= goalValue
-	default:
-		return false
-	}
-}
-
-// ConvertToMetricViewModel Converts a model to ViewModel that matches fields needed by the frontend
-func ConvertToMetricViewModel(goalType string, value float64, threshold float64, name string, comparison string, unit string, isOffDay bool) types.MetricViewModel {
-	var displayValue string
-	var displayThreshold string
-	// Handle weird float/int diff between goals
-	switch unit {
-	case "L":
-		displayValue = strconv.FormatFloat(value, 'f', 2, 64) + "L"
-		displayThreshold = strconv.FormatFloat(threshold, 'f', 2, 64) + "L"
-
-	default:
-		displayValue = strconv.Itoa(int(value))
-		displayThreshold = strconv.Itoa(int(threshold))
-	}
-
-	return types.MetricViewModel{
-		Value:            value,
-		DisplayValue:     displayValue,
-		Threshold:        threshold,
-		DisplayThreshold: displayThreshold,
-		Name:             name,
-		Success:          IsMetricSuccessful(value, threshold, comparison, isOffDay),
-		IsOff:            isOffDay,
-		Progression:      getProgression(value, threshold),
-		Picto:            getMetricPicto(goalType),
-	}
-}
-
-// ConvertToWorkoutMetricViewModel Converts a workout related metric to ViewModel that matches fields needed by the frontend
-func ConvertToWorkoutMetricViewModel(goalType string, duration float64, goalValue float64, name string, comparison string, isOffDay bool) types.MetricViewModel {
-	return types.MetricViewModel{
-		Value:            duration,
-		DisplayValue:     ConvertMsToHour(duration),
-		Threshold:        goalValue,
-		DisplayThreshold: ConvertMsToHour(goalValue),
-		Name:             name,
-		Success:          IsMetricSuccessful(duration, goalValue, comparison, isOffDay),
-		IsOff:            isOffDay,
-		Progression:      getProgression(duration, goalValue),
-		Picto:            getMetricPicto(goalType),
-	}
-}
-
 // getMetricPicto Used to display picto in dashboard
 func getMetricPicto(goalType string) string {
 	switch goalType {
@@ -107,46 +45,9 @@ func getMetricPicto(goalType string) string {
 
 // getProgression Used to display progress bar in dashboard
 func getProgression(value float64, threshold float64) int {
-
 	progression := int(value / threshold * 100)
-
 	if progression > 100 {
 		progression = 100
 	}
-
 	return progression
-}
-
-// CompareMetricsWithGoals Check if goal is achieved, off or failed for each metric
-func CompareMetricsWithGoals(metrics []models.Metric, workouts *[]models.Workout) ([]types.MetricViewModel, error) {
-	var comparedMetrics []types.MetricViewModel
-	// First, get all active goals
-	var goals []models.Goal
-	database.GetDB().Where("active = ?", true).Find(&goals)
-
-	for _, g := range goals {
-		var result types.MetricViewModel
-		isOff := IsOff(g.ID)
-
-		// Populate metric based on its type
-		switch g.Type {
-		case enums.MainWorkoutDuration:
-			duration := CalculateMainWorkoutDuration(workouts)
-			result = ConvertToWorkoutMetricViewModel(g.Type, duration, g.Value, "Gym duration", g.Comparison, isOff)
-		case enums.ExtraWorkoutDuration:
-			duration := CalculateExtraWorkoutDuration(workouts)
-			result = ConvertToWorkoutMetricViewModel(g.Type, duration, g.Value, "Cardio duration", g.Comparison, isOff)
-		default:
-			// Find linked metric thanks to goalID
-			idx := slices.IndexFunc(metrics, func(m models.Metric) bool {
-				return m.GoalID == g.ID
-			})
-			m := metrics[idx]
-			result = ConvertToMetricViewModel(g.Type, m.Value, g.Value, g.Name, g.Comparison, g.Unit, isOff)
-		}
-
-		comparedMetrics = append(comparedMetrics, result)
-	}
-
-	return comparedMetrics, nil
 }
--- server/pkg/types/metric.go
+++ server/pkg/types/metric.go
@@ -1,17 +1,5 @@
 package types
 
-type MetricViewModel struct {
-	Name             string  `json:"name"`
-	Value            float64 `json:"value"`
-	DisplayValue     string  `json:"displayValue"`
-	Threshold        float64 `json:"threshold"`
-	DisplayThreshold string  `json:"displayThreshold"`
-	Success          bool    `json:"success"`
-	IsOff            bool    `json:"isOff"`
-	Progression      int     `json:"difference"`
-	Picto            string  `json:"picto"`
-}
-
 type MetricInputModel struct {
 	Value float64 `json:"value"`
 	Type  string  `json:"type"`
