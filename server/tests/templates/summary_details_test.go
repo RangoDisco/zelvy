6e1e4ced33bd9f37b13b2a91674786ebd6be44cc
RangoDisco <diasmaxime1@gmail.com>
2025-07-18 18:25:28 +0200

    fix(templates): renamed summary's metric property to goal

--- server/tests/templates/summary_details_test.go
+++ server/tests/templates/summary_details_test.go
@@ -31,33 +31,33 @@ func TestDisplay(t *testing.T) {
 		t.Fatalf("Expected data-testid attribute to be rendered, but it wasn't")
 	}
 
-	// Expect the metrics to be rendered
-	if actualMetricsCount := doc.Find(`[data-testid="summaryDetailsTemplateMetric"]`).Length(); actualMetricsCount != len(testSummary.Metrics) {
-		t.Fatalf("Expected %d metrics to be rendered, but %d found", len(testSummary.Metrics), actualMetricsCount)
+	// Expect the goals to be rendered
+	if actualMetricsCount := doc.Find(`[data-testid="summaryDetailsTemplateMetric"]`).Length(); actualMetricsCount != len(testSummary.Goals) {
+		t.Fatalf("Expected %d metrics to be rendered, but %d found", len(testSummary.Goals), actualMetricsCount)
 	}
 
-	// Expect the metrics to contains the correct values
+	// Expect the goals to contain the correct values
 	doc.Find(`[data-testid="summaryDetailsTemplateMetric"]`).Each(func(i int, sel *goquery.Selection) {
 		// Check name
-		expectedName := testSummary.Metrics[i].Name
+		expectedName := testSummary.Goals[i].Name
 		if actualName := sel.Find(`[data-testid="summaryDetailsTemplateMetricName"]`).Text(); actualName != expectedName {
 			t.Fatalf("Expected metric name to be %q, but got %q", expectedName, actualName)
 		}
 
 		// Check picto
-		expectedPicto := testSummary.Metrics[i].Picto
+		expectedPicto := testSummary.Goals[i].Picto
 		if actualPicto := sel.Find(`[data-testid="summaryDetailsTemplateMetricPicto"]`).Text(); actualPicto != expectedPicto {
 			t.Fatalf("Expected metric picto to be %q, but got %q", expectedPicto, actualPicto)
 		}
 
 		// Check display value
-		expectedDisplayValue := testSummary.Metrics[i].DisplayValue
+		expectedDisplayValue := testSummary.Goals[i].DisplayValue
 		if actualDisplayValue := sel.Find(`[data-testid="summaryDetailsTemplateMetricValue"]`).Text(); actualDisplayValue != expectedDisplayValue {
 			t.Fatalf("Expected metric display value to be %q, but got %q", expectedDisplayValue, actualDisplayValue)
 		}
 
-		// Check threshold
-		expectedThreshold := testSummary.Metrics[i].DisplayThreshold
+		// Check the threshold
+		expectedThreshold := testSummary.Goals[i].DisplayThreshold
 		if actualThreshold := sel.Find(`[data-testid="summaryDetailsTemplateMetricThreshold"]`).Text(); actualThreshold != expectedThreshold {
 			t.Fatalf("Expected metric threshold to be %q, but got %q", expectedThreshold, actualThreshold)
 		}
