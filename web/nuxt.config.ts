e9c08a17f1ed34f8e5efc50a29d1f6a365b8f9eb
RangoDisco <diasmaxime1@gmail.com>
2025-09-08 20:20:26 +0200

    feat(web): added tailwind & daisyui

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    css: ["~/assets/css/app.css"],
 })
