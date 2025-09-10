c642083982bdd40038501637f355fb8a481cc22f
RangoDisco <diasmaxime1@gmail.com>
2025-09-10 19:11:44 +0200

    feat(home): added ViewSelector & basic class to layout

--- web/app/app.vue
+++ web/app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-base-100 min-h-screen container mx-auto p-6 md:mx-none flex flex-col gap-6">
+    <ViewSelector/>
+    <NuxtPage/>
   </div>
 </template>
+<script setup lang="ts">
+</script>
