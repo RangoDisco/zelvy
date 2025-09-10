2ca0d8ded4b431bd4dc508d146ae9bd8b061986d
RangoDisco <diasmaxime1@gmail.com>
2025-09-10 19:10:58 +0200

    feat(web): started view selector component

---
+++ web/app/components/ViewSelector.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="join w-full bg-base-200 rounded-full">
+    <input
+        class="join-item btn rounded-full w-1/2 h-8"
+        type="radio"
+        name="views"
+        aria-label="Daily"
+        checked
+    />
+    <input
+        class="join-item btn rounded-full w-1/2 h-8"
+        type="radio"
+        name="views"
+        aria-label="Overview"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
