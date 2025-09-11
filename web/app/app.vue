142b3ef07ef2176f5551dee52dcaf48ce8503651
RangoDisco <diasmaxime1@gmail.com>
2025-09-11 08:19:40 +0200

    feat(home): updated layout html structure

--- web/app/app.vue
+++ web/app/app.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-base-100 min-h-screen container mx-auto p-6 md:mx-none flex flex-col gap-6">
+  <main class="bg-base-100 min-h-screen container mx-auto p-6 md:mx-none flex flex-col gap-6">
     <ViewSelector/>
     <NuxtPage/>
-  </div>
+  </main>
 </template>
 <script setup lang="ts">
 </script>
