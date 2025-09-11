3d34dfeedb968673356ef2a8b47844a70f522277
RangoDisco <diasmaxime1@gmail.com>
2025-09-11 07:53:16 +0200

    feat(workout): started home workout card component

---
+++ web/app/components/WorkoutCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <article data-testid="workoutsTemplateWorkout" class="flex items-center justify-between w-full bg-base-200 rounded-lg h-[70px] px-4">
+    <div class="w-full flex gap-4 items-center">
+      <img
+          src=""
+          alt="wor"
+          class="w-10 h-10 rounded-md"
+      />
+      <div>
+        <p data-testid="workoutsTemplateWorkoutName" class="text-lg text-base-content">Pull 2</p>
+        <p data-testid="workoutsTemplateWorkoutDuration" class="text-md text-base-content">1h07</p>
+      </div>
+    </div>
+    <div data-testid="workoutsTemplateWorkoutActivityType" class="self-end pb-2 text-base">Gym</div>
+  </article>
+</template>
+
+<style scoped>
+
+</style>
