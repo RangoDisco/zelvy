a087ea3e34f50fbb645dae8bf9aa4509d3cb2b2b
RangoDisco <diasmaxime1@gmail.com>
2025-09-17 22:13:44 +0200

    feat(web): display goal's infos from props

--- web/app/components/home/daily/GoalCard.vue
+++ web/app/components/home/daily/GoalCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const props = defineProps(["goal"]);
+const goal = props.goal;
 </script>
 
 <template>
@@ -7,11 +8,11 @@ const props = defineProps(["goal"]);
            class="carousel-item bg-base-200 flex-col px-4 justify-evenly rounded-lg aspect-square w-[45%]
            md:rounded-none md:px-2 md:aspect-auto md:h-56 md:w-1/5">
     <p data-testid="summaryDetailsTemplateMetricName" class="text-base text-primary-content/50 md:text-lg">
-      Kcal burned</p>
-    <h3 data-testid="summaryDetailsTemplateMetricValue" class="text-3xl text-base-content">1032</h3>
+      {{ goal.name }}</p>
+    <h3 data-testid="summaryDetailsTemplateMetricValue" class="text-3xl text-base-content">{{ goal.displayValue }}</h3>
     <div class="flex flex-col gap-1">
       <p class="opacity-80 text-sm md:text-lg">Goal: <span
-          data-testid="summaryDetailsTemplateMetricThreshold">1000</span></p>
+          data-testid="summaryDetailsTemplateMetricThreshold">{{goal.displayThreshold}}</span></p>
       <div class="relative h-2 w-full">
         <div
             class="bg-primary h-2 rounded-l-full absolute top-0 left-0"></div>
