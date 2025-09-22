9902ef5d5aa690f5b5b472eef638a49d78354e6e
RangoDisco <diasmaxime1@gmail.com>
2025-09-22 22:50:07 +0200

    chore(web): removed nuxt project

--- web/app/app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <main class="bg-base-100 min-h-screen container mx-auto p-6 md:mx-none flex flex-col gap-6">
-    <NuxtPage/>
-  </main>
-</template>
-<script setup lang="ts">
-</script>
