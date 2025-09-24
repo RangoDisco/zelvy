822c0570a2f265497d1fc181123b4e05ea25b2b1
RangoDisco <diasmaxime1@gmail.com>
2025-09-24 20:22:02 +0200

    feat(web): added ViewSelector component

---
+++ web/src/lib/ui/ViewSelector.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {page} from "$app/state";
+</script>
+
+<section class="join w-full bg-base-200 rounded-full">
+    <a
+            href="/daily"
+            class="join-item btn rounded-full w-1/2 h-8 {page.url.pathname === '/daily' ? 'bg-primary' : ''}"
+    >
+        Daily
+    </a>
+    <a
+            href="/overview"
+            class="join-item btn rounded-full w-1/2 h-8 {page.url.pathname === '/overview' ? 'bg-primary' : ''}"
+    >
+        Overview
+    </a>
+</section>
