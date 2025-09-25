2b16b27fc254e2425805d9d4e8f92237688491c3
RangoDisco <diasmaxime1@gmail.com>
2025-09-25 07:51:38 +0200

    feat(web): added goal card and workout list item to daily page

--- web/src/routes/daily/+page.svelte
+++ web/src/routes/daily/+page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
     import type {PageProps} from "./$types";
-    import ViewSelector from "$lib/ui/ViewSelector.svelte";
+    import GoalCard from "$lib/ui/home/GoalCard.svelte";
+    import WorkoutListItem from "$lib/ui/home/WorkoutListItem.svelte";
 
     let {data}: PageProps = $props();
 </script>
 
 
-<main>
-    <ViewSelector/>
-    <p>{data.summary.id}</p>
-</main>
+<section class="flex flex-col gap-6">
+    <p>{data.summary.day}</p>
+    <section class="carousel w-full bg-base-900 gap-2 md:gap-0 md:rounded-lg">
+        {#each data.summary.goals as goal}
+            <GoalCard goal={goal}/>
+        {/each}
+    </section>
+    <section class="flex flex-col gap-2 w-full md:mx-auto">
+        {#each data.summary.workouts as workout (workout.id)}
+            <WorkoutListItem workout={workout}/>
+        {/each}
+    </section>
+</section>
