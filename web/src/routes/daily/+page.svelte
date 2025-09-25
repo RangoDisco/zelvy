9ccaa92c46ff3e69247cfee3faf54ca73ffcecf5
RangoDisco <diasmaxime1@gmail.com>
2025-09-25 04:58:56 +0200

    feat(web): started daily page

---
+++ web/src/routes/daily/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type {PageProps} from "./$types";
+    import ViewSelector from "$lib/ui/ViewSelector.svelte";
+
+    let {data}: PageProps = $props();
+</script>
+
+
+<main>
+    <ViewSelector/>
+    <p>{data.summary.id}</p>
+</main>
