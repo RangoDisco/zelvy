4cc2c2940bbe71b91f1815c5e7c8b9237d89c1e9
RangoDisco <diasmaxime1@gmail.com>
2025-09-25 07:50:36 +0200

    feat(web): added workout list item

---
+++ web/src/lib/ui/home/WorkoutListItem.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import {WorkoutViewModel} from "$lib/gen/zelvy/workout/workout_view_model";
+
+    type Props = {
+        workout: WorkoutViewModel
+    }
+
+    let {workout}: Props = $props();
+</script>
+
+<article data-testid="workoutsTemplateWorkout"
+         class="flex items-center justify-between w-full bg-base-200 rounded-lg h-[70px] px-4">
+    <div class="w-full flex gap-4 items-center">
+        <!--        TODO: HANDLE PICTURE-->
+        <img
+                src=""
+                alt="wor"
+                class="w-10 h-10 rounded-md"
+        />
+        <div>
+            <p data-testid="workoutsTemplateWorkoutName" class="text-lg text-base-content">{workout.name}</p>
+            <p data-testid="workoutsTemplateWorkoutDuration" class="text-md text-base-content">{workout.duration}</p>
+        </div>
+    </div>
+    <div data-testid="workoutsTemplateWorkoutActivityType"
+         class="self-end pb-2 text-base">{ workout.activityType.toLowerCase()}</div>
+</article>
+
+
