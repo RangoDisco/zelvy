6ab123b0e1c3d0d35199b817b0ab3a9b2ace9504
RangoDisco <diasmaxime1@gmail.com>
2025-09-25 18:09:40 +0200

    fix(web): used new GetSummaryRequest

--- web/src/routes/daily/+page.server.ts
+++ web/src/routes/daily/+page.server.ts
@@ -1,7 +1,7 @@
 import type {PageServerLoad} from "./$types";
 import {credentials} from "@grpc/grpc-js";
 import {SummaryServiceClient} from "$lib/gen/zelvy/summary/summary_service";
-import {GetSummaryResquest} from "$lib/gen/zelvy/summary/get_summary_request";
+import {GetSummaryRequest} from "$lib/gen/zelvy/summary/get_summary_request";
 import type {GetSummaryResponse} from "$lib/gen/zelvy/summary/get_summary_response";
 import {createMetadataWithAuth} from "$lib/server/grpc";
 
@@ -10,7 +10,7 @@ export const csr = false;
 export const load: PageServerLoad = async ({params}): Promise<{ summary: GetSummaryResponse }> => {
 
     const client = new SummaryServiceClient("localhost:50051", credentials.createInsecure());
-    const req = GetSummaryResquest.create();
+    const req = GetSummaryRequest.create();
     const res: GetSummaryResponse = await new Promise((resolve, reject) => {
         client.getSummary(req, createMetadataWithAuth(), (err, response) => {
             if (err) reject(err);
