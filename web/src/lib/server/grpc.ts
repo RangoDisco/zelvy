0dbe218276c10ec11ed989d6692a6a9823cd93ff
RangoDisco <diasmaxime1@gmail.com>
2025-09-25 07:28:26 +0200

    feat(web): updated metadata helper name

--- web/src/lib/server/grpc.ts
+++ web/src/lib/server/grpc.ts
@@ -2,7 +2,7 @@ import {API_KEY} from "$env/static/private";
 import {Metadata} from "@grpc/grpc-js";
 
 
-export const getMetadataWithAuth = () => {
+export const createMetadataWithAuth = () => {
     const metadata = new Metadata();
     metadata.add("authorization", API_KEY);
 
