7e1f1411e956a3622f6731c24537daaf0e04f69f
RangoDisco <diasmaxime1@gmail.com>
2025-09-25 07:51:20 +0200

    feat(web): moved view selector to layout and updated container style

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import "../app.css";
+    import favicon from "$lib/assets/favicon.svg";
+    import ViewSelector from "$lib/ui/ViewSelector.svelte";
 
-	let { children } = $props();
+    let {children} = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon}/>
 </svelte:head>
-
-{@render children?.()}
+<section class="bg-base-100 min-h-screen container mx-auto p-6 md:mx-none flex flex-col gap-6">
+    <ViewSelector/>
+    {@render children?.()}
+</section>
