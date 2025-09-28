b6d0252e6dd2c4ee6fc9ab359f9ec621afda1b1d
RangoDisco <diasmaxime1@gmail.com>
2025-09-28 18:46:55 +0200

    feat(home): updated layout max width for desktop

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import "../app.css";
     import favicon from "$lib/assets/favicon.svg";
-    import ViewSelector from "$lib/ui/ViewSelector.svelte";
 
     let {children} = $props();
 </script>
@@ -11,6 +10,6 @@
     <title>Zelvy dashboard</title>
     <meta name="description" content="Zelvy dashboard"/>
 </svelte:head>
-<section class="bg-base-100 min-h-screen container mx-auto p-6 md:mx-none flex flex-col gap-6">
+<section class="bg-base-100 min-h-screen container mx-auto p-6 md:max-w-[1200px] flex flex-col gap-6">
     {@render children?.()}
 </section>
