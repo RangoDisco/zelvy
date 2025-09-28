89ff271586a13b7bb2fdb77041f0af5c0e3d13c0
RangoDisco <diasmaxime1@gmail.com>
2025-09-28 18:46:01 +0200

    feat(home): updated desktop style for ViewSelector.svelte

---
+++ web/src/lib/ui/home/ViewSelector.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {page} from "$app/state";
+</script>
+
+<section class="join w-full md:w-1/5 bg-base-200 rounded-full">
+    <a
+            href="/daily"
+            class="text-base font-medium join-item btn md:btn-sm rounded-full w-1/2 {page.url.pathname === '/daily' ? 'bg-primary' : ''}"
+    >
+        Daily
+    </a>
+    <a
+            href="/overview"
+            class="text-base font-medium join-item btn md:btn-sm rounded-full w-1/2 {page.url.pathname === '/overview' ? 'bg-primary' : ''}"
+    >
+        Overview
+    </a>
+</section>
