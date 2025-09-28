a64b12283c47815f7d15cd3d5f11fcc3dadd81b3
RangoDisco <diasmaxime1@gmail.com>
2025-09-28 18:31:44 +0200

    feat(overview): added overview screen

---
+++ web/src/routes/overview/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type {PageProps} from "./$types";
+    import WinnerListItem from "$lib/ui/home/WinnerListItem.svelte";
+    import LeaderboardFilter from "$lib/ui/home/LeaderboardFilter.svelte";
+    import ViewSelector from "$lib/ui/ViewSelector.svelte";
+
+    const {data}: PageProps = $props();
+
+</script>
+
+
+<section class="flex flex-col gap-6">
+    <ViewSelector/>
+    <section class="w-full self-start flex gap-2">
+        <LeaderboardFilter label="This month" picto="calendar"/>
+        <LeaderboardFilter label="Overall" picto="sort"/>
+    </section>
+    <section class="flex flex-col gap-3 w-full md:mx-auto">
+        {#each data.winners as winner, i}
+            <WinnerListItem winner={winner} index={i}/>
+        {/each}
+    </section>
+</section>
