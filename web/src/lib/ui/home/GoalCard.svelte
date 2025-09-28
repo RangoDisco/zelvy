0d7734985eae74ee05b3479ed7800ec438cdceaa
RangoDisco <diasmaxime1@gmail.com>
2025-09-28 18:30:28 +0200

    feat(goal): updated text sizes

--- web/src/lib/ui/home/GoalCard.svelte
+++ web/src/lib/ui/home/GoalCard.svelte
@@ -9,14 +9,14 @@
 </script>
 
 <article data-testid="summaryDetailsTemplateMetric"
-         class="carousel-item bg-base-200 flex-col px-4 justify-evenly rounded-lg aspect-square w-[45%]
+         class="carousel-item bg-base-200 flex-col px-4 justify-evenly rounded-lg aspect-square w-[43%]
            md:rounded-none md:px-2 md:aspect-auto md:h-56 md:w-1/5">
-    <p data-testid="summaryDetailsTemplateMetricName" class="text-base text-primary-content/50 md:text-lg">
+    <p data-testid="summaryDetailsTemplateMetricName" class="text-base text-base-content/60 md:text-lg">
         {goal.name}
     </p>
-    <h3 data-testid="summaryDetailsTemplateMetricValue" class="text-3xl text-base-content">{goal.displayValue}</h3>
+    <h2 data-testid="summaryDetailsTemplateMetricValue" class="text-3xl font-semibold">{goal.displayValue}</h2>
     <div class="flex flex-col gap-1">
-        <p class="opacity-80 text-sm md:text-lg">Goal: <span
+        <p class="text-base-content/60 text-sm md:text-lg">Goal: <span
                 data-testid="summaryDetailsTemplateMetricThreshold">{goal.displayThreshold}</span></p>
         <div class="relative h-2 w-full">
             <div class="bg-primary h-2 rounded-l-full absolute top-0 left-0 {goal.progression === 100 ? 'rounded-r-full' : ''}"
