3325c5e8770afc9a18139d11a810685d29828b2d
RangoDisco <diasmaxime1@gmail.com>
2025-11-06 18:14:47 +0100

    feat(web): added default date util

---
+++ web/src/lib/utils/setDefaultDateRangeParams.ts
@@ -0,0 +1,11 @@
+import {redirect} from "@sveltejs/kit";
+
+export default function setDefaultDateRangeParams(url: URL) {
+    const endDate = new Date();
+    const startDate = new Date(endDate.setMonth(endDate.getMonth() - 1));
+
+    url.searchParams.set("end_date", endDate.toISOString().slice(0, 10));
+    url.searchParams.set("start_date", startDate.toISOString().slice(0, 10));
+
+    redirect(307, `${url.pathname}?${url.searchParams.toString()}`);
+}
