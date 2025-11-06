6a5736880a921b1d81abbdd893b71317500d9fcd
RangoDisco <diasmaxime1@gmail.com>
2025-11-06 18:14:16 +0100

    feat(user): added user lib

---
+++ web/src/lib/server/grpc/user.ts
@@ -0,0 +1,26 @@
+import {UserServiceClient} from "$lib/gen/zelvy/user/user_service";
+import {credentials} from "@grpc/grpc-js";
+import {GetWinnersRequest} from "$lib/gen/zelvy/user/get_winners_request";
+import type {GetWinnersResponse} from "$lib/gen/zelvy/user/get_winners_response";
+import {createMetadataWithAuth} from "$lib/server/grpc/metadata";
+
+const LIMIT = 10;
+
+export const getWinners = async (formattedSD: string, formattedED: string) => {
+    const client = getClient();
+    const winnerReq = GetWinnersRequest.create();
+    winnerReq.endDate = formattedED;
+    winnerReq.startDate = formattedSD;
+    winnerReq.limit = LIMIT;
+
+    return await new Promise((resolve, reject) => {
+        client.getWinners(winnerReq, createMetadataWithAuth(), (err, response) => {
+            if (err) reject(err);
+            else resolve(response);
+        });
+    }) as GetWinnersResponse;
+};
+
+const getClient = () => {
+    return new UserServiceClient("localhost:50051", credentials.createInsecure());
+};
