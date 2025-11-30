ee246859ec89a3e3ac88fa37b600846917f1baf1
RangoDisco <diasmaxime1@gmail.com>
2025-11-30 12:41:06 +0100

    feat(heatmap): handled queries and mobile version

---
+++ web/src/lib/ui/overview/HeatmapItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { HeatmapItemViewModel } from '$lib/gen/zelvy/summary/heatmap_item_view_model';
+
+	type Props = {
+		item: HeatmapItemViewModel;
+	}
+
+	const { item }: Props = $props();
+	let color = $state();
+	if (item.success === true) {
+		color = item.id !== '' ? 'bg-secondary' : 'bg-secondary/50';
+	} else {
+		color = 'bg-red-500';
+	}
+
+</script>
+
+<a href="/daily?date={item.date}" aria-label="date's details"
+	 class="min-h-8 max-h-10 aspect-square bg-secondary rounded-md {color}"></a>
