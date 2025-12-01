8abddaa041976235861a311d69cce1c4c0f9670b
RangoDisco <diasmaxime1@gmail.com>
2025-12-01 10:45:15 +0100

    feat(overview): moved full leaderboard to its own page and added winner and success rate stat cards

--- web/src/routes/overview/+page.svelte
+++ web/src/routes/overview/+page.svelte
@@ -1,36 +1,38 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
-	import WinnerListItem from '$lib/ui/home/WinnerListItem.svelte';
-	import LeaderboardFilter from '$lib/ui/home/LeaderboardFilter.svelte';
 	import ViewSelector from '$lib/ui/home/ViewSelector.svelte';
 	import HeatmapItem from '$lib/ui/overview/HeatmapItem.svelte';
+	import OverviewStatCard from '$lib/ui/overview/OverviewStatCard.svelte';
+	import type { HeatmapItemViewModel } from '$lib/gen/zelvy/summary/heatmap_item_view_model';
 
 	const { data }: PageProps = $props();
 	const { winRes, hmRes } = data;
 	const rowsNumber = hmRes.items.length > 50 ? 7 : 4;
 	const gridTemplate = `grid-rows-${rowsNumber} grid-cols-${Math.ceil(hmRes.items.length / rowsNumber)}`;
+	const winner = winRes.winners[0];
+
+	const successNumber = hmRes.items.filter((item: HeatmapItemViewModel) => item.success === true).length;
+	const successRate = successNumber / hmRes.items.length * 100;
+
 </script>
 
 <svelte:head>
 	<title>Zelvy dashboard - Overview</title>
 	<meta name="description" content="Stats overview" />
 </svelte:head>
-
 <section class="flex flex-col gap-6">
 	<ViewSelector />
-	<section class="w-full self-start flex gap-2">
-		<LeaderboardFilter label="This month" picto="calendar" />
-		<LeaderboardFilter label="Overall" picto="sort" />
+	<section class="flex flex-row justify-center flex-wrap gap-3">
+		<OverviewStatCard picto="👑" title={winner.username} subtitle="Most wins" value={winner.wins} />
+		<OverviewStatCard picto="🎯" title={`${successRate}%`} subtitle="Success rate"
+											value={`${successNumber}/${hmRes.items.length}`} />
+		<OverviewStatCard picto="O" title="KanaPei" subtitle="Most wins" value="35" />
+		<OverviewStatCard picto="O" title="KanaPei" subtitle="Most wins" value="35" />
 	</section>
 	<section
-		class="bg-base-200 grid grid-flow-col {gridTemplate} gap-1 rounded-lg overflow-scroll p-4">
+		class="min-h-12 bg-base-200 grid grid-flow-col {gridTemplate} gap-1 rounded-lg overflow-scroll p-4">
 		{#each hmRes.items as item (item.date)}
 			<HeatmapItem item={item} />
-		{/each}
-	</section>
-	<section class="flex flex-col gap-3 w-full md:mx-auto">
-		{#each winRes.winners as winner, i (winner.username)}
-			<WinnerListItem winner={winner} index={i} />
 		{/each}
 	</section>
 </section>
---
+++ web/src/routes/overview/leaderboard/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+	import LeaderboardFilter from '$lib/ui/home/LeaderboardFilter.svelte';
+	import WinnerListItem from '$lib/ui/home/WinnerListItem.svelte';
+</script>
+
+<svelte:head>
+	<title>Zelvy dashboard - Leaderboard</title>
+	<meta name="description" content="Leaderboard" />
+</svelte:head>
+<section class="w-full self-start flex gap-2">
+	<LeaderboardFilter label="This month" picto="calendar" />
+	<LeaderboardFilter label="Overall" picto="sort" />
+</section>
+
+<section class="flex flex-col gap-3 w-full md:mx-auto">
+	{#each winRes.winners as winner, i (winner.username)}
+		<WinnerListItem winner={winner} index={i} />
+	{/each}
+</section>
