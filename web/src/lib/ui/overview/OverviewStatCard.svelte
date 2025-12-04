230a8fae91f49c19e224ca1ee447a02e548b5175
RangoDisco <diasmaxime1@gmail.com>
2025-12-04 07:50:58 +0100

    feat(overview): added stat card component

--- web/src/lib/ui/overview/OverviewStatCard.svelte
+++ web/src/lib/ui/overview/OverviewStatCard.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	type Props = {
-		picto: string;
-		title: string;
-		subtitle: string;
-		value: string;
-	}
+    type Props = {
+        picto: string;
+        title: string;
+        subtitle: string;
+        value: string | number;
+    }
 
-	const { picto, title, subtitle, value }: Props = $props();
+    const {picto, title, subtitle, value}: Props = $props();
 
 </script>
 
 <article class="w-[48%] flex flex-col bg-base-200 rounded-lg p-4">
-	<div class="flex flex-row flex-wrap items-center gap-1">
-		<p>{picto}</p>
-		<p class="font-bold">{title}</p>
-	</div>
-	<p class="text-sm text-base-content/60">{subtitle}</p>
-	<p class="text-sm font-semibold">{value}</p>
+    <div class="flex flex-row flex-wrap items-center gap-1">
+        <p>{picto}</p>
+        <p class="font-bold">{title}</p>
+    </div>
+    <p class="text-sm text-base-content/60">{subtitle}</p>
+    <p class="text-sm font-semibold">{value}</p>
 </article>
