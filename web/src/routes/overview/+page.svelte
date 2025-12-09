b46dbd41698d736a7ee8da75a382c6b44b8ef7f7
RangoDisco <diasmaxime1@gmail.com>
2025-12-09 17:24:51 +0100

    feat(overview): update desktop style

--- web/src/routes/overview/+page.svelte
+++ web/src/routes/overview/+page.svelte
@@ -8,6 +8,7 @@
     import {getNewPeriodTitle, handlePeriodChange} from "$lib/utils/periodChanger";
     import {parseWorkouts} from "$lib/utils/chartFormatter";
     import Bar from "$lib/ui/charts/Bar.svelte";
+    import {ArrowLeft, ArrowRight} from "@lucide/svelte";
 
     const {data}: PageProps = $props();
     let rowsNumber = $state(data.hmRes.items.length > 50 ? 7 : 4);
@@ -26,21 +27,13 @@
     <section class="flex flex-row justify-between items-center">
         <h2 class="text-xl md:text-3xl">{period}</h2>
         <div class="flex flex-row gap-2">
-            <button class="btn btn-circle" aria-label="previous period"
+            <button class="btn btn-square" aria-label="previous period"
                     onclick={() => {handlePeriodChange(page.url, "previous")}}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5"
-                     stroke="currentColor" class="size-[1.2em]">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
-                </svg>
+                <ArrowLeft/>
             </button>
-            <button class="btn btn-circle" aria-label="next period"
+            <button class="btn btn-square" aria-label="next period"
                     onclick={() => {handlePeriodChange(page.url, "next")}}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5"
-                     stroke="currentColor" class="size-[1.2em]">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
-                </svg>
+                <ArrowRight/>
             </button>
         </div>
     </section>
@@ -53,7 +46,7 @@
             <OverviewStatCard picto="O" title="KanaPei" subtitle="Most wins" value="35"/>
         </section>
     </section>
-    <section class="flex flex-row flex-wrap justify-center md:justify-between gap-6 md:gap-2">
+    <section class="flex flex-row flex-wrap md:items-start justify-center md:justify-between gap-6 md:gap-2">
         <section class="w-full flex flex-col gap-2 md:w-[49%]">
             <h3 class="text-lg md:text-2xl">Heatmap</h3>
             <section
@@ -63,7 +56,7 @@
                 {/each}
             </section>
         </section>
-        <section class="flex flex-col gap-2 md:w-[49%] p-1">
+        <section class="flex flex-col gap-2 w-full md:w-[49%] p-1">
             <h3 class="text-lg md:text-2xl">Workouts</h3>
             <Bar data={workoutRadarData}/>
         </section>
