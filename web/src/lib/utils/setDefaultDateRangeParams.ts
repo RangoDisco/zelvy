305ce7f1688c00a3d852fab164dec336be31976a
RangoDisco <diasmaxime1@gmail.com>
2025-12-09 20:10:45 +0100

    feat(overview): updated period to 3 months

--- web/src/lib/utils/setDefaultDateRangeParams.ts
+++ web/src/lib/utils/setDefaultDateRangeParams.ts
@@ -3,7 +3,7 @@ import {redirect} from "@sveltejs/kit";
 export default function setDefaultDateRangeParams(url: URL) {
     const endDate = new Date();
     const startDate = new Date();
-    startDate.setMonth(endDate.getMonth() - 1);
+    startDate.setMonth(endDate.getMonth() - 3);
 
     url.searchParams.set("end_date", endDate.toISOString().slice(0, 10));
     url.searchParams.set("start_date", startDate.toISOString().slice(0, 10));
