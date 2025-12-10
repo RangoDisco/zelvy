9e89b812ec5ab57f8a4f8749f639bd22fd0eb42f
RangoDisco <diasmaxime1@gmail.com>
2025-12-10 20:50:32 +0100

    feat(goal): added icon to card and rework style

---
+++ web/src/lib/ui/home/GoalIcon.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import {CookingPot, Droplet, Dumbbell, Flame, HeartPulse, CircleQuestionMark} from "@lucide/svelte";
+    import {GoalType, goalTypeToJSON} from "$lib/gen/zelvy/goal/goal_type_enum";
+
+    type Props = {
+        type: string
+        index: number
+    }
+
+    const {type, index}: Props = $props();
+    const backgroundColor = index % 2 === 0 ? "bg-primary/30" : "bg-secondary/30";
+    const color = index % 2 === 0 ? "text-primary" : "text-secondary";
+    let IconComponent = $state(CircleQuestionMark);
+    switch (type) {
+        case goalTypeToJSON(GoalType.KCAL_CONSUMED):
+            IconComponent = CookingPot;
+            break;
+        case goalTypeToJSON(GoalType.MAIN_WORKOUT_DURATION):
+            IconComponent = Dumbbell;
+            break;
+        case goalTypeToJSON(GoalType.EXTRA_WORKOUT_DURATION):
+            IconComponent = HeartPulse;
+            break;
+        case goalTypeToJSON(GoalType.KCAL_BURNED):
+            IconComponent = Flame;
+            break;
+        case goalTypeToJSON(GoalType.MILLILITER_DRANK):
+            IconComponent = Droplet;
+            break;
+    }
+
+</script>
+
+
+<div class="hidden lg:flex w-14 h-14 justify-center items-center rounded-lg {backgroundColor}">
+    <IconComponent class={color}/>
+</div>
