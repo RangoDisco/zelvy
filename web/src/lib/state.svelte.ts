3f952fe09fa5985031a649945821585c93d3725a
RangoDisco <diasmaxime1@gmail.com>
2025-12-10 19:41:11 +0100

    feat(web): handled navigation change loading state

---
+++ web/src/lib/state.svelte.ts
@@ -0,0 +1,3 @@
+export const navigationStates = $state({
+    isLoading: false,
+});
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,8 +2,18 @@
     import "../app.css";
     import favicon from "$lib/assets/favicon.svg";
     import ViewSelector from "$lib/ui/home/ViewSelector.svelte";
+    import {afterNavigate, beforeNavigate} from "$app/navigation";
+    import {navigationStates} from "$lib/state.svelte";
 
     let {children} = $props();
+
+    beforeNavigate(() => {
+        navigationStates.isLoading = true;
+    });
+
+    afterNavigate(() => {
+        navigationStates.isLoading = false;
+    });
 </script>
 
 <svelte:head>
@@ -11,6 +21,7 @@
     <title>Zelvy dashboard</title>
     <meta name="description" content="Zelvy dashboard"/>
 </svelte:head>
+
 <section class="bg-base-100 min-h-screen container mx-auto p-6 md:max-w-[1200px] flex flex-col gap-6">
     <ViewSelector/>
     {@render children?.()}
