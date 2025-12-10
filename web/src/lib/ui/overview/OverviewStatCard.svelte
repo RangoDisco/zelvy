c392fda0be7f3caf555bbca2b15af0c32b9bde19
RangoDisco <diasmaxime1@gmail.com>
2025-12-10 09:23:51 +0100

    feat(overview): decreased stat card title size

--- web/src/lib/ui/overview/OverviewStatCard.svelte
+++ web/src/lib/ui/overview/OverviewStatCard.svelte
@@ -10,10 +10,10 @@
 
 </script>
 
-<article class="w-[48%] md:w-[32%] flex flex-col bg-base-200 rounded-lg p-4">
-    <div class="flex flex-row flex-wrap items-center gap-1">
-        <p>{picto}</p>
-        <p class="font-bold">{title}</p>
+<article class="w-[48%] md:w-[32%] flex flex-col bg-base-200 rounded-lg p-4 pr-0">
+    <div class="flex flex-row items-center gap-1">
+        <p class="text-sm lg:text-md">{picto}</p>
+        <p class="text-sm lg:text-md font-bold">{title}</p>
     </div>
     <p class="text-sm text-base-content/60">{subtitle}</p>
     <p class="text-sm font-semibold">{value}</p>
