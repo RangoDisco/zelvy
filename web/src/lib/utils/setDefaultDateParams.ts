3ae4c25be7f33f524784cef5db44e23475093dcc
RangoDisco <diasmaxime1@gmail.com>
2025-12-15 21:26:05 +0100

    feat(overview): refacto date searchParams handling

--- web/src/lib/utils/setDefaultDateParams.ts
+++ web/src/lib/utils/setDefaultDateParams.ts
@@ -1,6 +1,7 @@
 import {redirect} from "@sveltejs/kit";
+import {isDate} from "node:util/types";
 
-export default function validateOrUpdateDateParams(url: URL, isRange: boolean | null = false) {
+export const setDefaultDateParams = (url: URL, isRange: boolean | null = false) => {
     const startDate = new Date();
     const endDate = new Date();
 
@@ -14,4 +15,37 @@ export default function validateOrUpdateDateParams(url: URL, isRange: boolean | 
     }
 
     redirect(307, `${url.pathname}?${url.searchParams.toString()}`);
-}
+};
+
+export const getFormattedDateParams = (url: URL) => {
+    const eDParam = url.searchParams.get("end_date");
+    const sDParam = url.searchParams.get("start_date");
+
+    if (eDParam === null || sDParam === null) {
+        return {
+            formattedED: "",
+            formattedSD: "",
+            isInvalid: true,
+        };
+    }
+
+    const endDate = new Date(eDParam);
+    const startDate = new Date(sDParam);
+
+    if (startDate > endDate || !isDate(endDate) || !isDate(startDate)) {
+        return {
+            formattedED: "",
+            formattedSD: "",
+            isInvalid: true,
+        };
+    }
+
+    const formattedED = endDate.toISOString().slice(0, 10);
+    const formattedSD = startDate.toISOString().slice(0, 10);
+
+    return {
+        formattedED,
+        formattedSD,
+        isInvalid: false
+    };
+};
--- web/src/routes/overview/+page.server.ts
+++ web/src/routes/overview/+page.server.ts
@@ -1,8 +1,7 @@
 import type {PageServerLoad} from "./$types";
 import type {GetWinnersResponse} from "$lib/gen/zelvy/user/get_winners_response";
 import type {GetSummaryHeatmapResponse} from "$lib/gen/zelvy/summary/get_summary_heatmap_response";
-import setDefaultDateRangeParams from "$lib/utils/setDefaultDateParams";
-import {isDate} from "node:util/types";
+import {getFormattedDateParams, setDefaultDateParams} from "$lib/utils/setDefaultDateParams";
 import {getSummaryHeatmap} from "$lib/server/grpc/summary";
 import {getWinners} from "$lib/server/grpc/user";
 import {getWorkouts} from "$lib/server/grpc/workout";
@@ -14,24 +13,11 @@ export const load: PageServerLoad = async ({url}): Promise<{
     hmRes: GetSummaryHeatmapResponse,
     wkrRes: GetWorkoutsResponse
 } | null> => {
-    const eDParam = url.searchParams.get("end_date");
-    const sDParam = url.searchParams.get("start_date");
+    const {formattedSD, formattedED, isInvalid} = getFormattedDateParams(url);
 
-    if (eDParam === null || sDParam === null) {
-        setDefaultDateRangeParams(url, true);
-        return null;
+    if (isInvalid) {
+        setDefaultDateParams(url, true);
     }
-
-    const endDate = new Date(eDParam);
-    const startDate = new Date(sDParam);
-
-    if (startDate > endDate || !isDate(endDate) || !isDate(startDate)) {
-        setDefaultDateRangeParams(url, true);
-        return null;
-    }
-
-    const formattedED = endDate.toISOString().slice(0, 10);
-    const formattedSD = startDate.toISOString().slice(0, 10);
 
     const winnersResponse = await getWinners(formattedSD, formattedED);
     const heatmapResponse = await getSummaryHeatmap(formattedSD, formattedED);
