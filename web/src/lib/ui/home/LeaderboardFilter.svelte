f76af8812713930e43fb52b820138f7cb06a5c1d
RangoDisco <diasmaxime1@gmail.com>
2025-12-16 20:45:35 +0100

    feat(winners): handled filter change

--- web/src/lib/ui/home/LeaderboardFilter.svelte
+++ web/src/lib/ui/home/LeaderboardFilter.svelte
@@ -1,27 +1,64 @@
 <script lang="ts">
-    import {ArrowUpDown, CalendarDays} from "@lucide/svelte";
+    import {ArrowUpDown} from "@lucide/svelte";
+    import {page} from "$app/state";
+    import {WinnerFilterType, winnerFilterTypeToJSON} from "$lib/gen/zelvy/user/get_winners_request";
+    import {afterNavigate, goto, invalidateAll} from "$app/navigation";
 
-    type Props = {
-        label: string;
-        picto: string;
-    }
+    let isOpen = $state(false);
+    const initialFilter = $derived(page.url.searchParams.get("filter"));
+    let label = $state();
 
-    const {label, picto}: Props = $props();
-    let isOpen = $state(null);
+    afterNavigate(() => {
+        isOpen = false;
+        switch (initialFilter) {
+            case winnerFilterTypeToJSON(WinnerFilterType.RELEVANT):
+                label = "Relevant";
+                break;
+            case winnerFilterTypeToJSON(WinnerFilterType.IRRELEVANT):
+                label = "Irrelevant";
+                break;
+            default:
+                label = "Overall";
+        }
+    });
+
+    const handleFilterChange = (value: WinnerFilterType | null) => {
+        if (value === initialFilter) {
+            return;
+        }
+
+        if (value) {
+            const filterValue = winnerFilterTypeToJSON(value);
+            page.url.searchParams.set("filter", filterValue);
+        } else {
+            page.url.searchParams.delete("filter");
+        }
+
+        goto(page.url).then(() => {
+            invalidateAll().then(() => {
+            });
+        });
+    };
 </script>
 
 <details class="dropdown" open={isOpen}>
     <summary class="btn rounded-full bg-primary/60">
-        {#if picto === "calendar"}
-            <CalendarDays size="20"/>
-        {:else}
-            <ArrowUpDown size="20"/>
-        {/if}
+        <ArrowUpDown size="20"/>
         {label}
     </summary>
-    <ul class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 shadow-sm">
+    <ul class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 shadow-sm gap-2">
         <li>
-            <button disabled class="btn">Soon</button>
+            <button class="btn" onclick={() => handleFilterChange(null)}>Overall</button>
+        </li>
+        <li>
+            <button class="btn" onclick={() => handleFilterChange(WinnerFilterType.RELEVANT)}>Relevant (real
+                wins)
+            </button>
+        </li>
+        <li>
+            <button class="btn" onclick={() => handleFilterChange(WinnerFilterType.IRRELEVANT)}>Irrelevant
+                (no money)
+            </button>
         </li>
     </ul>
 
