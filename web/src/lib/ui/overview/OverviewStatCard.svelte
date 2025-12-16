898a804117cd6c75be23c0986cdd4f529a1bfe26
RangoDisco <diasmaxime1@gmail.com>
2025-12-16 20:52:27 +0100

    feat(overview): added link to leaderboard

--- web/src/lib/ui/overview/OverviewStatCard.svelte
+++ web/src/lib/ui/overview/OverviewStatCard.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import {ArrowRight} from "@lucide/svelte";
+
     type Props = {
         picto: string;
         title: string;
@@ -7,8 +9,7 @@
         link: string | null;
     }
 
-    const {picto, title, subtitle, value}: Props = $props();
-
+    const {picto, title, subtitle, value, link}: Props = $props();
 </script>
 
 <article class="w-[48%] md:w-[32%] flex flex-col bg-base-200 rounded-lg p-4 pr-0">
@@ -17,5 +18,12 @@
         <p class="text-sm lg:text-lg font-bold">{title}</p>
     </div>
     <p class="text-sm lg:text-md text-base-content/60">{subtitle}</p>
-    <p class="text-sm lg:text-md font-semibold">{value}</p>
+    <div class="flex flex-row items-center justify-between gap-1 pr-4">
+        <p class="text-sm lg:text-md font-semibold">{value}</p>
+        {#if link !== null}
+            <a href={link} target="_blank" rel="noopener noreferrer" class="bg-primary/50 rounded-full p-1">
+                <ArrowRight size="16"/>
+            </a>
+        {/if}
+    </div>
 </article>
