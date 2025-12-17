9a4187bfef884090ca146f213b5b9ae2f82649a8
RangoDisco <diasmaxime1@gmail.com>
2025-12-17 18:31:24 +0100

    feat(winners): added GetWinners request test

--- server/tests/handlers/user_service_test.go
+++ server/tests/handlers/user_service_test.go
@@ -2,12 +2,13 @@ package tests
 
 import (
 	"context"
+	"testing"
+	"time"
+
 	pb_usr "github.com/rangodisco/zelvy/gen/zelvy/user"
 	"github.com/rangodisco/zelvy/server/tests/factories"
 	"github.com/rangodisco/zelvy/server/tests/utils"
 	"github.com/stretchr/testify/assert"
-	"testing"
-	"time"
 )
 
 func TestAddUser(t *testing.T) {
@@ -22,5 +23,20 @@ func TestAddUser(t *testing.T) {
 	}
 
 	assert.Equal(t, resp.Message, "Upsert successful")
+}
 
+func TestGetWinners(t *testing.T) {
+	client := pb_usr.NewUserServiceClient(utils.Conn)
+	body := factories.CreateGetWinnersRequest()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	resp, err := client.GetWinners(ctx, body)
+	if err != nil {
+		t.Fatal(err, resp)
+	}
+
+	assert.GreaterOrEqual(t, 1, len(resp.Winners))
+	assert.GreaterOrEqual(t, int64(1), resp.Winners[0].Wins)
+	assert.NotEmpty(t, resp.Winners[0].Username)
 }
