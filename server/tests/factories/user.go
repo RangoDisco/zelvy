e7e02d4f953e31faadbc86cc3dce89e77405a907
RangoDisco <diasmaxime1@gmail.com>
2025-12-17 18:31:24 +0100

    feat(winners): added GetWinners request test

--- server/tests/factories/user.go
+++ server/tests/factories/user.go
@@ -1,11 +1,23 @@
 package factories
 
-import pb_usr "github.com/rangodisco/zelvy/gen/zelvy/user"
+import (
+	"time"
+
+	pb_usr "github.com/rangodisco/zelvy/gen/zelvy/user"
+)
 
 func CreateAddUserRequest() *pb_usr.AddUserRequest {
 	return &pb_usr.AddUserRequest{
 		Username:    "User123",
 		PaypalEmail: "email@email.com",
 		DiscordId:   "123",
+	}
+}
+
+func CreateGetWinnersRequest() *pb_usr.GetWinnersRequest {
+	return &pb_usr.GetWinnersRequest{
+		StartDate: time.Now().Format("2006-01-02"),
+		EndDate:   time.Now().Add(48 * time.Hour).Format("2006-01-02"),
+		Limit:     2,
 	}
 }
