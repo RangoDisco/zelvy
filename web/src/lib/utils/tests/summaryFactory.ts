814d49de95b1712f3b45812d4a88320f765a0a47
RangoDisco <diasmaxime1@gmail.com>
2025-12-20 07:54:29 +0100

    feat(daily): added page and component tests

--- web/src/lib/utils/tests/summaryFactory.ts
+++ web/src/lib/utils/tests/summaryFactory.ts
@@ -1,4 +1,10 @@
 import type {GetSummaryHeatmapResponse} from "$lib/gen/zelvy/summary/get_summary_heatmap_response";
+import type {GetSummaryResponse} from "$lib/gen/zelvy/summary/get_summary_response";
+import {GoalType, goalTypeToJSON} from "$lib/gen/zelvy/goal/goal_type_enum";
+import {
+    WorkoutActivityType,
+    workoutActivityTypeToJSON
+} from "$lib/gen/zelvy/workout/workout_activity_type_enum";
 
 export const mockGetHeatmapResponse = (): GetSummaryHeatmapResponse => {
     return {
@@ -10,4 +16,41 @@ export const mockGetHeatmapResponse = (): GetSummaryHeatmapResponse => {
             }
         ]
     };
+};
+
+export const mockGetSummaryResponse = (): GetSummaryResponse => {
+    return {
+        id: "1",
+        day: new Date().toISOString(),
+        winner: {
+            name: "winner",
+            discordId: "123"
+        },
+        workouts: [
+            {
+                id: "1",
+                name: "Gym",
+                kcalBurned: 295,
+                picto: "",
+                doneAt: new Date().toISOString(),
+                duration: "3784",
+                activityType: workoutActivityTypeToJSON(WorkoutActivityType.STRENGTH)
+            }
+        ],
+        goals: [
+            {
+                type: goalTypeToJSON(GoalType.KCAL_BURNED),
+                name: "Kcal burned",
+                isOff: false,
+                value: 578,
+                displayValue: "578",
+                threshold: 500,
+                displayThreshold: "500",
+                progression: 100,
+                picto: "",
+                isSuccessful: true
+            }
+        ]
+    };
+
 };
